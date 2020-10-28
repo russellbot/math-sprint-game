2f37a04f7a237efa0ec1c367bf075c2e0c558bb2
Russell Hart <russellbot@gmail.com>
2020-10-28 16:20:44 -0400

    countdown page

--- script.js
+++ script.js
@@ -83,6 +83,21 @@ function createEquations() {
 //   itemContainer.appendChild(bottomSpacer);
 // }
 
+// Displays 3, 2, 1, Go!
+function countdownStart() {
+  countdown.textContent = '3';
+  setTimeout(() => countdown.textContent = '2', 1000);
+  setTimeout(() => countdown.textContent = '1', 2000);
+  setTimeout(() => countdown.textContent = 'Go!', 3000);
+}
+
+// Navigate from Splash Page to Countdown Page
+function showCountdown() {
+  countdownPage.hidden = false;
+  splashPage.hidden = true;
+  countdownStart();
+}
+
 // Get the value from selected radio button
 function getRadioValue() {
   let radioValue;
@@ -99,6 +114,9 @@ function selectQuestionAmount(e) {
   e.preventDefault();
   questionAmount = getRadioValue();
   console.log('question amount:', questionAmount);
+  if (questionAmount) {
+    showCountdown();
+  }  
 }
 
 startForm.addEventListener('click', () => {
